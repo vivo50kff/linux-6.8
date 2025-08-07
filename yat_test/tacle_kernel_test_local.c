0bfa4409d14f17d6b450e3b3f81977021188ad6d
ma <3218649584@qq.com>
2025-08-07 15:10:48 +0800

    perf工具配置

--- yat_test/tacle_kernel_test_local.c
+++ yat_test/tacle_kernel_test_local.c
@@ -46,11 +46,11 @@ long timeval_diff_ms(struct timeval *start, struct timeval *end) {
 int main() {
     printf("=== 本地 TacleBench Kernel 12任务集周期测试 ===\n");
     // 父进程设置YAT_CASCHED调度策略
-    // if (set_yat_scheduler(0, 0) == 0) {
-    //     printf("父进程PID=%d 成功设置为YAT_CASCHED调度策略\n", getpid());
-    // } else {
-    //     printf("父进程PID=%d 设置YAT_CASCHED失败，使用默认策略\n", getpid());
-    // }
+    if (set_yat_scheduler(0, 0) == 0) {
+        printf("父进程PID=%d 成功设置为YAT_CASCHED调度策略\n", getpid());
+    } else {
+        printf("父进程PID=%d 设置YAT_CASCHED失败，使用默认策略\n", getpid());
+    }
     struct timeval total_start, total_end;
     gettimeofday(&total_start, NULL);
     long task_total[NUM_TASKS] = {0};
